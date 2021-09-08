d37a7277dcd9b53c831f390ca2cfd41a4bc3888e
JIVitian <jivitian@gmail.com>
2021-09-07 22:07:01 -0300

    Cambios menores

--- TL2-TP3/Models/DeliveryMan.cs
+++ TL2-TP3/Models/DeliveryMan.cs
@@ -7,5 +7,10 @@ namespace TL2_TP3.Models
 {
     public class DeliveryMan
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public List<Order> orders { get; }
     }
 }
