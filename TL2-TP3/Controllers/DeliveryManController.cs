c188acac01f14e1d537de6e158bf076de7c8da4b
JIVitian <jivitian@gmail.com>
2021-09-22 17:33:36 -0300

    Inyección de dependencia de nlog e implementación en el controlador de cadetes

--- TL2-TP3/Controllers/DeliveryManController.cs
+++ TL2-TP3/Controllers/DeliveryManController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using NLog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,15 +13,18 @@ namespace TL2_TP3.Controllers
     {
 
         private readonly ILogger<DeliveryManController> _logger;
+        private readonly Logger nlog;
 
-        public DeliveryManController(ILogger<DeliveryManController> logger)
+        public DeliveryManController(ILogger<DeliveryManController> logger, Logger nlog)
         {
             _logger = logger;
+            this.nlog = nlog;
         }
 
         // GET: DeliveryManController
         public ActionResult Index()
         {
+            nlog.Info("DeliveryMan Index.");
             return View();
         }
 
@@ -43,10 +47,12 @@ namespace TL2_TP3.Controllers
         {
             try
             {
+                nlog.Info("New Delivery Man Created.");
                 return RedirectToAction(nameof(Index));
             }
             catch
             {
+                nlog.Error("Delviery Man could not be created.");
                 return View();
             }
         }
@@ -64,10 +70,12 @@ namespace TL2_TP3.Controllers
         {
             try
             {
+                nlog.Info("Delivery Man Updated.");
                 return RedirectToAction(nameof(Index));
             }
             catch
             {
+                nlog.Error("Delviery Man could not be Updated.");
                 return View();
             }
         }
@@ -85,10 +93,12 @@ namespace TL2_TP3.Controllers
         {
             try
             {
+                nlog.Info("Delivery Man Deleted.");
                 return RedirectToAction(nameof(Index));
             }
             catch
             {
+                nlog.Error("Delviery Man could not be Deleted.");
                 return View();
             }
         }
--- TL2-TP3/Startup.cs
+++ TL2-TP3/Startup.cs
@@ -23,6 +23,7 @@ namespace TL2_TP3
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(NLog.LogManager.GetCurrentClassLogger());
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
         }
 
