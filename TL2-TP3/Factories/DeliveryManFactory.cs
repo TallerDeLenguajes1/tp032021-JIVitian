24bcd574c1eb7eeddc4991427820af6b71a359d4
JIVitian <jivitian@gmail.com>
2021-09-23 17:31:29 -0300

    Carpetas Factories y Repositories Creadas

---
+++ TL2-TP3/Factories/DeliveryManFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TL2_TP3.Factories
+{
+    public class DeliveryManFactory
+    {
+
+    }
+}
