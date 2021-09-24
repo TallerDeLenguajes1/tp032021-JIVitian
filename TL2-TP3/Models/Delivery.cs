d78663fb84539a82577a6a53b2f47b1e18811535
JIVitian <jivitian@gmail.com>
2021-09-24 18:56:59 -0300

    Añadida Clase Delivery

---
+++ TL2-TP3/Models/Delivery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TL2_TP3.Models
+{
+    public class Delivery
+    {
+        string Name { get; set; }
+        List<DeliveryBoy> DeliveryBoyList { get; }
+
+        public Delivery ()
+        {
+            DeliveryBoyList = new List<DeliveryBoy>();
+        }
+    }
+}
