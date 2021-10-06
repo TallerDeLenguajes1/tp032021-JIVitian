c8d46c1cc0c16f5aa3c6249fd5d7e5810513d290
JIVitian <jivitian@gmail.com>
2021-10-05 21:30:07 -0300

    Probando multiples archivos

--- TL2-TP3/Repositories/OrderRepository.cs
+++ TL2-TP3/Repositories/OrderRepository.cs
@@ -1,11 +1,45 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TL2_TP3.Models;
 
 namespace TL2_TP3.Repositories
 {
-    public class OrderRepository
+    public class OrderRepository : RepositoryJSON<Order>
     {
+        public readonly DeliveryRepository delivery;
+        public OrderRepository(DeliveryRepository delivery)
+        {
+            path = "Orders.JSON";
+            List = ReadJSON();
+            this.delivery = delivery;
+        }
+
+        public void AddOrder(IFormCollection collection)
+        {
+            var order = new Order
+            {
+                Number = List.Count > 0 ? List.Last().Number + 1 : 1,
+                Observation = collection["Observation"],
+                State = 0, // Initialize in ToConfirm
+                Client = new Client { Name = "Juan Perez", Address = "Mikasa", Id = 1, Phone = "1234" }
+            };
+
+            List.Add(order);
+
+            delivery.AddOrder(int.Parse(collection["DeliveryBoy"]), order);
+
+            SaveJSON();
+        }
+
+        public void EditOrder(int id, IFormCollection collection)
+        {
+            var order = List.Find(x => x.Number == id);
+            order.Observation = collection["Observation"];
+            order.State = (State)int.Parse(collection["State"]);
+            SaveJSON();
+        }
     }
 }
---
+++ TL2-TP3/Repositories/RepositoryJSON.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TL2_TP3.Repositories
+{
+    public abstract class RepositoryJSON <T>
+    {
+        protected string path;
+
+        public List<T> List { get; set; }
+
+        protected List<T> ReadJSON()
+        {
+            if (File.Exists(path))
+            {
+                using (FileStream archivo = new FileStream(path, FileMode.Open))
+                {
+                    StreamReader strReader = new StreamReader(archivo);
+                    string json = strReader.ReadToEnd();
+                    strReader.Close();
+                    strReader.Dispose();
+                    return json != "" ? JsonSerializer.Deserialize<List<T>>(json) : new List<T>();
+                }
+            }
+            else
+            {
+                var archivo = new FileStream(path, FileMode.Create);
+                return new List<T>();
+            }
+        }
+
+        protected void SaveJSON()
+        {
+            string JSONData = JsonSerializer.Serialize(List);
+
+            using (FileStream file = new FileStream(path, FileMode.Create))
+            {
+                StreamWriter strWrite = new StreamWriter(file);
+                strWrite.WriteLine(JSONData);
+                strWrite.Close();
+                strWrite.Dispose();
+            }
+        }
+
+    }
+}
