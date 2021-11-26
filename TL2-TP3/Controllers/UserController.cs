b9be493972b9c4148fa445c8839cdcb453299fa9
JIVitian <jivitian@gmail.com>
2021-11-26 00:50:44 -0300

    Creando controlador y vistas de usuarios

---
+++ TL2-TP3/Controllers/UserController.cs
@@ -0,0 +1,71 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TL2_TP3.Repositories.SQLite;
+
+namespace TL2_TP3.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly Logger logger;
+        private readonly Repository repository;
+        private readonly IMapper mapper;
+
+        public UserController(
+            Logger logger,
+            Repository repository,
+            IMapper mapper)
+        {
+            this.logger = logger;
+            this.repository = repository;
+            this.mapper = mapper;
+        }
+
+        // GET: User/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: User/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: User/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: User/Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string user, string password)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
